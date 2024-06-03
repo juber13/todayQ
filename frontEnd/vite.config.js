14e6879ca0f4effa7befd28c6e691849a3c5fb9c
juber13 <juberk1010@gmail.com>
2024-06-03 16:20:11 +0530

    correct route

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http//localhost:5050",
-        secure: false,
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://todayq-backend-96id.onrender.com",
+  //       secure: false,
+  //     }
+  //   }
+  // },
   plugins: [react()],
 })
